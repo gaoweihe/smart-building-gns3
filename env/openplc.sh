a9030b6cdc38c9c2bf7bb0efc692b8f863a19573
GAO Weihe <gao.weihe@outlook.com>
2024-07-31 08:03:09 +0000

    add env shells

---
+++ env/openplc.sh
@@ -0,0 +1,4 @@
+sudo apt update
+git clone https://github.com/thiagoralves/OpenPLC_v3.git 
+cd OpenPLC_v3
+./install.sh linux 
---
+++ env/simulation.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo snap install docker 
+git clone https://github.com/gaoweihe/simulation-server-modbus-gateway.git 
+cd simulation-server-modbus-gateway 
+sudo docker compose up 
