47a4283923f59935a00c01dead69512b5a04a24e
GAO Weihe <gao.weihe@outlook.com>
2024-07-31 08:10:20 +0000

    update docker-compose shell

--- env/simulation.sh
+++ env/simulation.sh
@@ -2,4 +2,4 @@ sudo apt update
 sudo snap install docker 
 git clone https://github.com/gaoweihe/simulation-server-modbus-gateway.git 
 cd simulation-server-modbus-gateway 
-sudo docker compose up 
+sudo docker-compose up -d 
