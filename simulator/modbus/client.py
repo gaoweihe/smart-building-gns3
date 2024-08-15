27787b00a33ef11cd9bc9d4fa41a5a39d7c996ea
GAO Weihe <gao.weihe@outlook.com>
2024-08-15 14:33:31 +0000

    remove example comments

--- simulator/modbus/client.py
+++ simulator/modbus/client.py
@@ -1,31 +1,5 @@
 #!/usr/bin/env python3
-"""Pymodbus asynchronous client example.
 
-usage::
-
-    client_async.py [-h] [-c {tcp,udp,serial,tls}]
-                    [-f {ascii,rtu,socket,tls}]
-                    [-l {critical,error,warning,info,debug}] [-p PORT]
-                    [--baudrate BAUDRATE] [--host HOST]
-
-    -h, --help
-        show this help message and exit
-    -c, -comm {tcp,udp,serial,tls}
-        set communication, default is tcp
-    -f, --framer {ascii,rtu,socket,tls}
-        set framer, default depends on --comm
-    -l, --log {critical,error,warning,info,debug}
-        set log level, default is info
-    -p, --port PORT
-        set port
-    --baudrate BAUDRATE
-        set serial device baud rate
-    --host HOST
-        set host, default is 127.0.0.1
-
-The corresponding server must be started before e.g. as:
-    python3 server_sync.py
-"""
 import asyncio
 import logging
 import sys
