16817bd7916eee832c176277a2b98d4773ca4bbf
GAO Weihe <gao.weihe@outlook.com>
2024-08-16 01:47:40 +0000

    add simulator cloud-init scripts

---
+++ simulator/modbus/cloud-init/gateway/generate.sh
@@ -0,0 +1,1 @@
+genisoimage -output cloud-init.iso -volid cidata -joliet -rock user-data meta-data
---
+++ simulator/modbus/cloud-init/generate.sh
@@ -0,0 +1,9 @@
+cd ac/
+bash generate.sh
+cd ..
+cd gateway/
+bash generate.sh
+cd ..
+cd thermostat/
+bash generate.sh
+cd ..
